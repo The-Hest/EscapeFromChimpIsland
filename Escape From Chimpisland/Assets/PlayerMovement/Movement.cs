82b428c25225c7160add79bf512f017c3696d1b0
Simon Steen Pedersen <simon.steen.p@gmail.com>
2020-10-20 15:43:54 +0200

    Simple movement but bugged

--- Escape From Chimpisland/Assets/PlayerMovement/Movement.cs
+++ Escape From Chimpisland/Assets/PlayerMovement/Movement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float movementSpeed;
+    public Rigidbody2D rigidgebody;
+
+    private Vector2 moveDirection;
 
     // Update is called once per frame
     void Update()
     {
+        ProcessInput();
         
+    }
+
+    /// <summary>
+    /// Used for physics calculation as this is consitent 
+    /// </summary>
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY);
+    }
+
+    void Move()
+    {
+        rigidgebody.velocity = new Vector2(moveDirection.x * movementSpeed, moveDirection.y * movementSpeed);
     }
 }
