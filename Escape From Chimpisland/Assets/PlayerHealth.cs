5198e89fa8ecb3f70e8f52138e38029662206920
Simon Steen Pedersen <simon.steen.p@gmail.com>
2020-10-27 16:28:41 +0100

    Player Health system added

---
+++ Escape From Chimpisland/Assets/PlayerHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public HealthController healthController;
+    public DamageController damageController;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        healthController.playerHealth = 100;
+        healthController.UpdateHealthUI();
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        healthController.DamageTaken(damageController.damage);
+    }
+}
---
+++ Escape From Chimpisland/Assets/Scripts/Heath System/HealthController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    public Text healthUIText;
+    public int playerHealth;
+
+
+    public void HealingReceived(int healing)
+    {
+        playerHealth += healing;
+        UpdateHealthUI();
+    }
+
+    public void DamageTaken(int damage)
+    {
+        playerHealth -= damage;
+        UpdateHealthUI();
+    }
+
+    /// <summary>
+    /// Updates the Canvas with the Health Text element 
+    /// </summary>
+    public void UpdateHealthUI()
+    {
+        healthUIText.text = playerHealth.ToString();
+    }
+}
