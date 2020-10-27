adcfffbe978c05f1e65a98376fcda36d59d8fda8
Mikkel <mikkel.a.krohn@gmail.com>
2020-10-27 13:40:56 +0100

    Procedual dungeon generation added

---
+++ Escape From Chimpisland/Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    /*
+     1 -> Bot
+     2 -> Top
+     3 -> Left
+     4 -> Rigth
+     */
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+    void Start()
+    {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+    
+    // Update is called once per frame
+    void Spawn()
+    {
+        if (spawned == false)
+        {
+            switch (openingDirection)
+            {
+                case 1:
+                    rand = Random.Range(0, templates.bottomRooms.Length);
+                    Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+                    break;
+                case 2:
+                    rand = Random.Range(0, templates.topRooms.Length);
+                    Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+                    break;
+                case 3:
+                    rand = Random.Range(0, templates.leftRooms.Length);
+                    Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+                    break;
+                case 4:
+                    rand = Random.Range(0, templates.rightRooms.Length);
+                    Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+                    break;
+                default:
+                    print("ERROR - couldn't spawn room");
+                    break;
+
+            }
+            spawned = true;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+
+        if (other.CompareTag("SpawnPoint"))
+        {
+            if(other.GetComponent<RoomSpawner>().spawned == false && spawned == false)
+            {
+                Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            spawned = true;
+        }
+    }
+}
