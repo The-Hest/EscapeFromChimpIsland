d4bf1be14c5e834347e67cfe635d46916d944932
Mikkel <mikkel.a.krohn@gmail.com>
2020-11-24 13:13:30 +0100

    Musik :)

---
+++ Escape From Chimpisland/Assets/EnemySpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemieList;
+
+    private float randX;
+    private float randY;
+    private Vector2 spawnPoint;
+    private System.Random rand;
+    private int enemiesToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rand = new System.Random();
+        enemiesToSpawn = rand.Next(1, 7);
+        Debug.Log(transform.position);
+
+        for (int i = 0; i <= enemiesToSpawn; i++)
+        {
+            randX = Random.Range(8.5f, -8.5f); //Et rums bredde i unity units
+            randY = Random.Range(2.5f, -3.5f); //Et rums højde i unity units
+
+            spawnPoint = new Vector2(randX + transform.position.x, randY + transform.position.y);
+            Instantiate(enemieList, spawnPoint, Quaternion.identity);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
