a7076ec8d9d299b97186d4d3866b78bc1d2e3fa2
Mikkel <mikkel.a.krohn@gmail.com>
2020-12-11 22:20:37 +0100

    Addition to bossroom - traps

---
+++ Escape From Chimpisland/Assets/LSmallFlame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LSmallFlame : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float bulletForce = 20f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        InvokeRepeating("Shooting", 1f, 2.0f);
+
+
+    }
+
+    private void Shooting()
+    {
+        print("fuck");
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+
+}
---
+++ Escape From Chimpisland/Assets/smallFlameShoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smallFlameShoot : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float bulletForce = 20f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+          
+        
+        
+            
+            InvokeRepeating("Shooting",2.0f,2.0f);
+          
+            
+    }
+
+    private void Shooting()
+    {
+        print("fuck");
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+   // IEnumerator ExampleCoroutine()
+  //  {
+  //      yield return new WaitForSeconds(5);
+   // }
+}
