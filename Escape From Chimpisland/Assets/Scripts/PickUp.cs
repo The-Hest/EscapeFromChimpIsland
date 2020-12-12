8a3372bb626de68ee9c3b2e8e447202edd84fe9b
Simon Steen Pedersen <simon.steen.p@gmail.com>
2020-12-12 02:13:07 +0100

    New weapon sprites, weapon pickup added

--- Escape From Chimpisland/Assets/Scripts/PickUp.cs
+++ Escape From Chimpisland/Assets/Scripts/PickUp.cs
@@ -5,28 +5,25 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    private Inventory mInventory;
     public List<string> inventory;
     public GameObject itemButton;
-    private string itemType;
+    
+    private Inventory mInventory;
+    private GameObject _weapon;
 
     private void Start()
     {
         mInventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
-        
-        
+        _weapon = GameObject.Find("Gun");
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && gameObject.tag == "Collectibles")
         {
-
-
             if (Input.GetKey(KeyCode.E))
             {
-
-
                 for (int i = 0; i < mInventory.slots.Length; i++)
                 {
                     if (mInventory.isFull[i] == false)
@@ -37,23 +34,26 @@ public class PickUp : MonoBehaviour
                         Destroy(gameObject);
                         break;
                     }
-                   
                 }
+            }
+        }
+        else if (collision.CompareTag("Player") && gameObject.tag == "Weapon")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _weapon.GetComponent<SpriteRenderer>().sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+                _weapon.GetComponent<Shoot>().bulletPrefab = gameObject.GetComponent<Shoot>().bulletPrefab;
+                _weapon.GetComponent<Shoot>().bulletForce = gameObject.GetComponent<Shoot>().bulletForce;
             }
         }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && gameObject.tag == "Collectibles")
         {
-          
-
             if (Input.GetKey(KeyCode.E))
             {
-               
-                
-
                 for (int i = 0; i < mInventory.slots.Length; i++)
                 {
                     if (mInventory.isFull[i] == false)
@@ -64,8 +64,16 @@ public class PickUp : MonoBehaviour
                         Destroy(gameObject);
                         break;
                     }
-
                 }
+            }
+        }
+        else if (collision.CompareTag("Player") && gameObject.tag == "Weapon")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _weapon.GetComponent<SpriteRenderer>().sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+                _weapon.GetComponent<Shoot>().bulletPrefab = gameObject.GetComponent<WeaponHandler>().bulletPrefab;
+                _weapon.GetComponent<Shoot>().bulletForce = gameObject.GetComponent<WeaponHandler>().bulletForce;
             }
         }
     }
---
+++ Escape From Chimpisland/Assets/Scripts/Weapons/WeaponHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHandler : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float bulletForce;
+}
